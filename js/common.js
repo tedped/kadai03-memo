b7c2ef8baf4229d4013a3320b826379e0f44e1c1
tedped <grantee.andiron.8s@icloud.com>
2025-12-03 22:54:38 +0900

    20251203_update:README.md

--- js/common.js
+++ js/common.js
@@ -5,8 +5,8 @@ let week = ["日", "月", "火", "水", "木", "金", "土"];
 //日時取得
 let now = new Date(); //日付取得準備（必須）！！
 let year = now.getFullYear(); //年
-let month = now.getMonth() + 1; //月+１を足す
-let date = now.getDate(); //日
+let month = ("0" + now.getMonth() + 1).slice(-2); //月+１を足す
+let date = ("0" + now.getDate()).slice(-2); //日
 let day = now.getDay(); //曜日（数値）
 let h = ("0" + now.getHours()).slice(-2); //時
 let m = ("0" + now.getMinutes()).slice(-2); //分
